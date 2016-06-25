2be13e818fab85084805218079590269de8edd21
Ali-Amechghal <ali.amechghal@gmail.com>
2016-06-25 20:33:36 +0000

    switch case

--- symbol.js
+++ symbol.js
@@ -20,3 +20,32 @@ for (const x of iterableObject) {
 // Output:
 // hello
 // world
+
+
+//use sympbols in switch
+
+const COLOR_RED    = Symbol('Red');
+const COLOR_ORANGE = Symbol('Orange');
+const COLOR_YELLOW = Symbol('Yellow');
+const COLOR_GREEN  = Symbol('Green');
+const COLOR_BLUE   = Symbol('Blue');
+const COLOR_VIOLET = Symbol('Violet');
+
+function getComplement(color) {
+    switch (color) {
+        case COLOR_RED:
+            return COLOR_GREEN;
+        case COLOR_ORANGE:
+            return COLOR_BLUE;
+        case COLOR_YELLOW:
+            return COLOR_VIOLET;
+        case COLOR_GREEN:
+            return COLOR_RED;
+        case COLOR_BLUE:
+            return COLOR_ORANGE;
+        case COLOR_VIOLET:
+            return COLOR_YELLOW;
+        default:
+            throw new Exception('Unknown color: '+color);
+    }
+}
