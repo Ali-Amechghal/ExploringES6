df3bc4d78ba788f2c4b28201122db6c2c76cdb8e
Ali-Amechghal <ali.amechghal@gmail.com>
2016-06-27 23:56:03 +0000

    literal

---
+++ literal.js
@@ -0,0 +1,59 @@
+const tmpl = addrs => html`
+    <table>
+    ${addrs.map(addr => html`
+        <tr><td>$${addr.first}</td></tr>
+        <tr><td>$${addr.last}</td></tr>
+    `)}
+    </table>
+`;
+const data = [
+    { first: '<Jane>', last: 'Bond' },
+    { first: 'Lars', last: '<Croft>' },
+];
+console.log(tmpl(data));
+// Output:
+// <table>
+//
+//     <tr><td>&lt;Jane&gt;</td></tr>
+//     <tr><td>Bond</td></tr>
+//
+//     <tr><td>Lars</td></tr>
+//     <tr><td>&lt;Croft&gt;</td></tr>
+//
+// </table>
+
+function html(templateObject, ...substs) {
+    // Use raw template strings: we don’t want
+    // backslashes (\n etc.) to be interpreted
+    const raw = templateObject.raw;
+
+    let result = '';
+
+    substs.forEach((subst, i) => {
+        // Retrieve the template string preceding
+        // the current substitution
+        let lit = raw[i];
+
+        // In the example, map() returns an Array:
+        // If substitution is an Array (and not a string),
+        // we turn it into a string
+        if (Array.isArray(subst)) {
+            subst = subst.join('');
+        }
+
+        // If the substitution is preceded by a dollar sign,
+        // we escape special characters in it
+        if (lit.endsWith('$')) {
+            subst = htmlEscape(subst);
+            lit = lit.slice(0, -1);
+        }
+        result += lit;
+        result += subst;
+    });
+    // Take care of last template string
+    // (Never fails, because an empty tagged template
+    // produces one template string, an empty string)
+    result += raw[raw.length-1]; // (A)
+
+    return result;
+}
